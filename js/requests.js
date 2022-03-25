d7c9ea3e157e1d8fca8c373f246f9e3b12fb044c
J0vid <daponte08@gmail.com>
2022-03-25 02:03:48 -0600

    better styled plotly plot

--- js/requests.js
+++ js/requests.js
@@ -14,26 +14,56 @@ function getScores(){
 fetch('http://127.0.0.1:7181/similarity_scores?reference=' + encodeURIComponent(document.getElementById("referenceComp").value) + '&synd_comp=' + document.getElementById("syndromeComp").value + '&facial_subregion=' + network.getSelectedNodes()[0])
   .then(response => response.json())
   .then(data => {
-      console.log(data.subregion_scores.length)
-      console.log(data.subregion_scores[0].face_score)
-      syndSimilarity = document.getElementById('similarityScores');
+        console.log(data.subregion_scores.length)
+        console.log(data.subregion_scores[0].face_score)
+        syndSimilarity = document.getElementById('similarityScores');
 
-      subregionScores = [];
-      wholefaceScores = [];
-      syndNames = [];
+        subregionScores = [];
+        wholefaceScores = [];
+        syndNames = [];
 
-      for(var i = 0; i < data.subregion_scores.length; i++) {
-       subregionScores.push(data.subregion_scores[i].face_score)
-       wholefaceScores.push(data.wholeface_scores[i].face_score)
-       syndNames.push(data.subregion_scores[i].Syndrome)
+        for(var i = 0; i < data.subregion_scores.length; i++) {
+        subregionScores.push(data.subregion_scores[i].face_score)
+        wholefaceScores.push(data.wholeface_scores[i].face_score)
+        syndNames.push(data.subregion_scores[i].Syndrome)
         }
 
-      Plotly.newPlot( syndSimilarity, [{
-      x: subregionScores,
-      y: wholefaceScores,
-      mode: 'markers',
-      type: 'scatter',
-      text: syndNames}],{
-      margin: { t: 0 } });
+        //figure out 5 biggest/smallest subregion scores and annotate
+
+        var scoreData = [{
+            x: subregionScores,
+            y: wholefaceScores,
+            mode: 'markers',
+            type: 'scatter',
+            text: syndNames,
+            marker: {
+                color: '#48298C'
+            }
+        }]
+
+        var layout = {
+            margin: { t: 0 }, 
+            autosize: true,
+            xaxis: {
+              title: {
+                text: 'Subregion scores',
+                font: {
+                  size: 15,
+                  color: '#48298C'
+                }
+              },
+            },
+            yaxis: {
+              title: {
+                text: 'Face scores',
+                font: {
+                  size: 15,
+                  color: '#48298C'
+                }
+              }
+            }
+          };
+
+        Plotly.newPlot(syndSimilarity, scoreData, layout);
     });
 }
